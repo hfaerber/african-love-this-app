cbab15f7f312603b86df70d9799196fd03127019
Heather Faerber <heatherdfaerber@gmail.com>
2020-01-09 15:44:24 -0700

    Set up userReducer tests

---
+++ src/reducers/userReducer.test.js
@@ -0,0 +1,9 @@
+import { userReducer } from './userReducer';
+
+describe('userReducer', () => {
+  it('should return the initial state', () => {
+    const expected = '';
+    const result = userReducer(undefined, {});
+    expect(result).toEqual(expected);
+  })
+})
