2ff16e21272572f9831ac2762c83f6d805e258d1
Heather Faerber <heatherdfaerber@gmail.com>
2020-01-09 11:41:58 -0700

    Add unit tests

--- src/WelcomeForm/WelcomeForm.test.js
+++ src/WelcomeForm/WelcomeForm.test.js
@@ -1,3 +1,27 @@
 import React from 'react';
 import WelcomeForm from './WelcomeForm';
 import { shallow } from 'enzyme';
+
+describe('WelcomeForm', () => {
+  let wrapper, mockEvent;
+
+  beforeEach(() => {
+    wrapper = shallow(<WelcomeForm />);
+    mockEvent = { target: { value: 'test value' } };
+  })
+
+  it('should invoke handleChange on change', () => {
+    wrapper.instance().handleChange = jest.fn();
+    wrapper.find('.input-name').simulate('change', mockEvent)
+    expect(wrapper.instance().handleChange).toHaveBeenCalledWith(mockEvent);
+  })
+
+  it('should update state when handleChange is invoked', () => {
+    let initialState = { name: 'initial state value' };
+    let expectedState = { name: 'test value' }
+    wrapper.setState(initialState);
+    wrapper.instance().handleChange(mockEvent);
+    expect(wrapper.state()).toEqual(expectedState);
+  })
+
+})
