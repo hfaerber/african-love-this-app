b8716b10b24489f995f056dfada76936ce866c60
Heather Faerber <heatherdfaerber@gmail.com>
2020-01-10 13:04:34 -0700

    Add reducer test for TOGGLE_STUDY_SESSION

--- src/reducers/countriesReducer.test.js
+++ src/reducers/countriesReducer.test.js
@@ -26,5 +26,26 @@ describe('countriesReducer', () => {
       {name: "Botswana", capital: "Gaborone", subregion: "Southern Africa", needsStudied: true}
     ];
     expect(result).toEqual(expectedState);
+  });
+
+  it('should return the updated state if type is TOGGLE_STUDY_STATUS', () => {
+    const testState = [
+      {name: "Algeria", capital: "Algiers", subregion: "Northern Africa", needsStudied: true},
+      {name: "Angola", capital: "Luanda", subregion: "Middle Africa", needsStudied: true},
+      {name: "Benin", capital: "Porto-Novo", subregion: "Western Africa", needsStudied: false},
+      {name: "Botswana", capital: "Gaborone", subregion: "Southern Africa", needsStudied: false}
+    ];
+    const action = {
+      type: 'TOGGLE_STUDY_STATUS',
+      name: 'Angola'
+    };
+    const result = countriesReducer(testState, action);
+    const expectedState = [
+      {name: "Algeria", capital: "Algiers", subregion: "Northern Africa", needsStudied: true},
+      {name: "Angola", capital: "Luanda", subregion: "Middle Africa", needsStudied: false},
+      {name: "Benin", capital: "Porto-Novo", subregion: "Western Africa", needsStudied: false},
+      {name: "Botswana", capital: "Gaborone", subregion: "Southern Africa", needsStudied: false}
+    ];
+    expect(result).toEqual(expectedState);
   })
 })
