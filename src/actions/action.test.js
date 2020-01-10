88b0ed8635411d8f05c1b30392632f2657fdb90d
Heather Faerber <heatherdfaerber@gmail.com>
2020-01-09 19:02:42 -0700

    Create and test updateCountries action creator

--- src/actions/action.test.js
+++ src/actions/action.test.js
@@ -7,8 +7,24 @@ describe('actions', () => {
       const expectedAction = {
         type:'UPDATE_USER',
         user: 'Ben'
-      }
+      };
       const result = actions.updateUser(name);
       expect(result).toEqual(expectedAction);
-    })
+  });
+
+  it('should return an object with an array of countries and type of UPDATE_COUNTRIES',
+    () => {
+    const array = [
+      {name: "Algeria", capital: "Algiers", subregion: "Northern Africa"},
+      {name: "Angola", capital: "Luanda", subregion: "Middle Africa"},
+      {name: "Benin", capital: "Porto-Novo", subregion: "Western Africa"},
+      {name: "Botswana", capital: "Gaborone", subregion: "Southern Africa"}
+    ];
+    const expectedAction = {
+      type: 'UPDATE_COUNTRIES',
+      countries: array
+    };
+    const result = actions.updateCountries(array);
+    expect(result).toEqual(expectedAction);
+  });
 })
--- src/actions/index.js
+++ src/actions/index.js
@@ -2,3 +2,8 @@ export const updateUser = user => ({
   type: 'UPDATE_USER',
   user
 });
+
+export const updateCountries = countries => ({
+  type: 'UPDATE_COUNTRIES',
+  countries
+});
