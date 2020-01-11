5f0c0dd38b4fbe9505603490334ed90721dc59c0
Heather Faerber <heatherdfaerber@gmail.com>
2020-01-10 21:18:20 -0700

    Add StudyForm for filtering cards

---
+++ src/StudyForm/StudyForm.js
@@ -0,0 +1,52 @@
+import React, {Component}  from 'react';
+import './StudyForm.css';
+import { connect } from 'react-redux';
+
+export class StudyForm extends Component {
+  constructor() {
+    super();
+      this.state = {
+        selectedFilter: 'All'
+      }
+  }
+
+  handleSelection = (event) => {
+    this.setState({ selectedFilter: event.target.value })
+
+
+
+    // user action/dispatch to update the selectedFilter key in STORE
+    // map in StudyContainer will rely on conditional to render based on selectedFilter key in state
+    // selectedCards key in STORE will be passed to StudyContainer and .map()
+    // will be run on that array so its creating and displaying cards for the selection
+// WILL IT RERENDER WITH SELECTEDFILTER LOCAL STATE AS ALL DEFAULT?
+// MAYBE THAT SHOULD ALSO BE A KEY IN STORE THAT IS PASSED AS PROP SO IT DOESN'T
+// DEFAULT BACK TO ALL ON EACH RE-RENDER???
+
+
+  }
+
+  render() {
+    return(
+      <label forhtml="display-cards-options" className='label-show'>Show:
+        <select name="show" id="display-cards-options" className='select-show'
+          value={this.state.selectedFilter}
+          onChange={(event) => this.handleSelection(event)}>
+          <option value="All">All</option>
+          <option value="Still Learning">Still Learning</option>
+          <option value="Got It!">Got It!</option>
+        </select>
+      </label>
+    )
+  }
+}
+
+export const mapStateToProps = state => ({
+
+})
+
+export const mapDispatchToProps = dispath => ({
+
+})
+
+export default connect(null, null)(StudyForm)
