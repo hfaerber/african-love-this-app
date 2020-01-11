168fde7942c033b02ca837948c6db94a7af8da0e
Heather Faerber <heatherdfaerber@gmail.com>
2020-01-11 16:53:39 -0700

    Add mapDispatchToProps test in App and comments for .catch test

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import { App } from './App';
+import { App, mapDispatchToProps } from './App';
 import { shallow } from 'enzyme';
 import { fetchCountryData } from '../apiCalls';
+import { updateCountries } from '../actions';
 
 jest.mock('../apiCalls');
 
@@ -25,14 +25,35 @@ describe('AppContainer', () => {
     });
 
     it('should invoke fetchCountryData after component mounts', () => {
-
       expect(fetchCountryData).toHaveBeenCalled();
+    });
+
+    it('should invoke updateCountries prop when fetchCountryData resolves', async () => {
+      await fetchCountryData();
+      expect(mockUpdateCountries).toHaveBeenCalled();
+    })
+
+    // it('should do something errorHandly if fetchCountryData rejects', () => {
+    //   fetchCountryData.mockImplementation(() => {
+    //     return Promise.reject((Error: 'got an error'))
+    //   });
+    //   fetchCountryData();
+    //   expect(console.log).toHaveBeenCalled();
+    // })
+
+  });
+
+  describe('mapDispatchToProps', () => {
+    it('should call dispatch with an updateCountries action when updateCountries is called', () => {
+      const mockCountries = [
+        {name: "Algeria", capital: "Algiers", subregion: "Northern Africa", needsStudied: true},
+        {name: "Benin", capital: "Porto-Novo", subregion: "Western Africa", needsStudied: true},
+      ];
+      const mockDispatch = jest.fn();
+      const actionToDispatch = updateCountries(mockCountries);
+      const mappedProps = mapDispatchToProps(mockDispatch);
+      mappedProps.updateCountries(mockCountries);
+      expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
     })
   })
 })
-
-test('renders learn react link', () => {
-  // const { getByText } = render(<App />);
-  // const linkElement = getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
-});
