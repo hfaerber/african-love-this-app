fc2408f96bd901f19546b4e870530c695fa5ac4a
Heather Faerber <heatherdfaerber@gmail.com>
2020-01-12 19:21:27 -0700

    Add hover and remove extra button from invalid route

--- src/NoMatch/NoMatch.js
+++ src/NoMatch/NoMatch.js
@@ -12,9 +12,6 @@ const NoMatch = () => {
         <h2 className='h2-invalid-route'>Wow. You really do need help with Geography.</h2>
         <h2 className='h2-not-found'>Page Not Found</h2>
         <h2 className='h2-invalid-route'>How did you even get here?</h2>
-        <Link to ='/'>
-          <button className='button-invalid'>Let's try again</button>
-        </Link>
       </div>
     </>
   )
